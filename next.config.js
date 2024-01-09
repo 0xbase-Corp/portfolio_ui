5c7a659dbd19968dcba3b1befb1a69d59108b0e4
fahad <fahadu8@gmail.com>
2024-01-09 21:06:58 +0500

    Add locales, routing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    i18n: {
+      locales: ['en-US', 'fr', 'nl-NL'],
+      defaultLocale: 'en-US',
+    },
+  }
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main >
+      <h1>Root Layout</h1>
+    </main>
+  )
+}
