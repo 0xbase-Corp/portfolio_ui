022b27ee76f4fd53d01e5e8e8886bcbd6cd889d9
fahad <fahadu8@gmail.com>
2024-01-14 22:00:24 +0500

    simplify workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = {
-    i18n: {
-      locales: ['en-US', 'fr', 'nl-NL'],
-      defaultLocale: 'en-US',
-    },
-  }
+const nextConfig = {
+  i18n: {
+  locales: ['en-US', 'fr', 'nl-NL'],
+  defaultLocale: 'en-US',
+},
+
+}
+
+module.exports = nextConfig
