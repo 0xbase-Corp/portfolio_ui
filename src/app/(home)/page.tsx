67cdb31487742041d1f75217b3c3bece3fe4a1cd
ItsFaizan <faizanjavid71@gmail.com>
2024-01-21 21:01:04 +0500

    Navbar with Material UI

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,18 @@
 import Button from '@mui/material/Button';
-import styles from '../styles/Home.module.css';  
+import styles from '../styles/Home.module.css'; 
+import Navbar from '../components/Navbar'; 
 
 export default function Home() {
   return (
     <main className={styles.container}>
+    <Navbar />
     <h1>Landing Home Page</h1>
     <p>This is to check if automatic changes are picked up</p>
     <p>Material UI Integrated</p>
     <h1>Welcome to my Next.js + Material-UI app!</h1>
     <Button
       variant="contained"
-      style={{ borderRadius: 50, padding: '8px 16px', backgroundColor: '#f50057', color: '#fff' }}
+      style={{ borderRadius: 50, padding: '8px 16px', backgroundColor: '#03CA9B', color: '#fff' }}
       className={styles.centerButton}  
     >
       Click me
