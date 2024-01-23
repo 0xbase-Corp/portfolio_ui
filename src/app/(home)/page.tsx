839daf4cd1d7ae0d56a2e1a2466a10ab7b9680bd
fahad <fahadu8@gmail.com>
2024-01-23 23:47:20 +0500

    Modal component ready

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,20 @@
-import Button from '@mui/material/Button'; 
-
+'use client'
+import Button from '@mui/material/Button';
+import MModal from '@/components/modal/modal';
 export default function Home() {
   return (
     <main >
-    <h1>Landing Home Page</h1>
-    <p>This is to check if automatic changes are picked up</p>
-    <p>Material UI Integrated</p>
-    <h1>Welcome to my Next.js + Material-UI app!</h1>
-    <Button
-      variant="contained"
-      style={{ borderRadius: 50, padding: '8px 16px', backgroundColor: '#03CA9B', color: '#fff' }}  
-    >
-      Click me
-    </Button>
-  </main>
+      <h1>Landing Home Page</h1>
+      <p>This is to check if automatic changes are picked up</p>
+      <p>Material UI Integrated</p>
+      <h1>Welcome to my Next.js + Material-UI app!</h1>
+      <Button
+        variant="contained"
+        style={{ borderRadius: 50, padding: '8px 16px', backgroundColor: '#03CA9B', color: '#fff' }}
+      >
+        Click me
+      </Button>
+      <MModal title="test" body="this is the body" ></MModal>
+    </main>
   )
 }
