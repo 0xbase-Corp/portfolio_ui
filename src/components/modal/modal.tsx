ca15ff11768c14abf526467bd8fc9cbf163206f8
Muhammad Faizan Javed <95418879+ItsFaizan@users.noreply.github.com>
2024-01-30 21:21:55 +0500

    Cross icon added and fixed closing of modal by clicking outside (#5)
    * Cross icon added and fixed closing of modal by clicking outside
    
    * Responsiveness fixed and made border rounded

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -2,20 +2,26 @@ import { FC, ReactNode, useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import { Modal as MaterialModal } from '@mui/material';
-
 
 const style = {
     position: 'absolute' as 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 240,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
+    borderRadius: 2,
     p: 4,
+    '@media (min-width:600px)': {
+        width: 400,  
+    },
 };
+
 interface ModalProps {
     openModalButton: string
     title: string
@@ -33,11 +39,23 @@ const Modal: FC<ModalProps> = ({ title, body, openModalButton, children }) => {
             <Button onClick={handleOpen}>{openModalButton}</Button>
             <MaterialModal
                 open={open}
-                onClose={handleClose}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
                 <Box sx={style}>
+                    <IconButton
+                        edge="end"
+                        color="inherit"
+                        onClick={handleClose}
+                        aria-label="close"
+                        sx={{
+                            position: 'absolute',
+                            top: 0,
+                            right: 20,
+                        }}
+                    >
+                        <CloseIcon />
+                    </IconButton>
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         {title}
                     </Typography>
@@ -51,4 +69,4 @@ const Modal: FC<ModalProps> = ({ title, body, openModalButton, children }) => {
     );
 }
 
-export default Modal
+export default Modal;
