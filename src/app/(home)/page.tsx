4f0776662854492fa2ecb0e4f280b46541a31fc8
ItsFaizan <faizanjavid71@gmail.com>
2024-01-31 22:11:33 +0500

    Login Modal Complete

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,17 @@
 'use client'
 
+import LoginDialog from "@/container/LoginDialog"
+
 
 export default function Home() {
   return (
     <main >
       <h1>Landing Home Page</h1>
+      <LoginDialog onSignupClick={function (): void {
+        throw new Error("Function not implemented.")
+      } } onConnectWalletClick={function (): void {
+        throw new Error("Function not implemented.")
+      } }  />
     </main>
   )
 }
---
+++ src/container/LoginDialog.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+import Modal from '@/components/modal/modal';
+import CustomDivider from '@/components/divider/CustomDivider';
+
+interface LoginProps {
+    onSignupClick: () => void;
+    onConnectWalletClick: () => void;
+}
+
+const LoginDialog: FC<LoginProps> = ( ) => {
+
+    return (
+        <Modal title="Login" openModalButton='Login'>
+
+            <TextField fullWidth label="Email" margin="normal" />
+            <TextField fullWidth label="Password" type="password" margin="normal" />
+            
+            <Typography sx={{ textAlign: 'right', mb: 2 }}>
+                Don't have an account? <Typography component="a" href='#' >Signup</Typography>
+            </Typography>
+
+            <Button variant="contained" color="success" fullWidth
+            sx={{ mb: 2}}
+            >
+                Login
+            </Button>
+
+            <CustomDivider title="OR"  />
+
+            
+            <Button variant="outlined" color="primary" fullWidth >
+                Connect Wallet
+            </Button>
+        </Modal>
+    );
+};
+
+export default LoginDialog;
