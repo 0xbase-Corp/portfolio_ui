7f5064ddfd75965c5f18ed7113c521207511cb31
ItsFaizan <faizanjavid71@gmail.com>
2024-02-02 21:39:12 +0500

    Issue fixed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,9 +7,9 @@ import LoginDialog from '@/views/LoginDialog'
 export default function Home() {
   const simulateAsyncTask = async () => {
     // Simulate a delay of 2 seconds
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    console.log('Async task completed!');
-  };
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    console.log('Async task completed!')
+  }
   return (
     <main>
       <h1>Landing Home Page</h1>
--- src/components/loadingbutton/LoadingButton.tsx
+++ src/components/loadingbutton/LoadingButton.tsx
@@ -1,36 +1,28 @@
-import React, { FC, useState } from 'react';
-import MuiButton from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress';
+import MuiButton from '@mui/material/Button'
+import CircularProgress from '@mui/material/CircularProgress'
+import React, { FC, useState } from 'react'
 
 interface ButtonProps {
-  title: string;
-  onClick: () => Promise<void>; // Assuming your button click triggers an asynchronous operation
+  title: string
+  onClick: () => Promise<void>
 }
 
 const Button: FC<ButtonProps> = ({ title, onClick }) => {
-  const [loading, setLoading] = useState(false);
-
-  const handleClick = async () => {
-    setLoading(true);
-    console.log('Button clicked');
-
-    try {
-      await onClick();
-    } finally {
-      setLoading(false);
-    }
-  };
+  const [loading, setLoading] = useState(false)
 
   return (
     <MuiButton
-      onClick={handleClick}
+      onClick={() => {
+        setLoading(true)
+        onClick().finally(() => setLoading(false))
+      }}
       disabled={loading}
       variant="contained"
       color="primary"
     >
       {loading ? <CircularProgress size={24} color="inherit" /> : title}
     </MuiButton>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
