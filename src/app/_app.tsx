bfb1fe484b369d53a32bb3a209c679e00bbc8039
fahad <fahadu8@gmail.com>
2024-02-06 03:47:12 +0500

    Add privy

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,5 +1,27 @@
+import { PrivyProvider } from '@privy-io/react-auth'
+
 import type { AppProps } from 'next/app'
 
 require('dotenv').config({ path: ['.env.local', '.env'] })
 
-export const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const handleLogin = (user: any) => {
+  console.log(`User ${user.id} logged in!`)
+}
+export const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      onSuccess={handleLogin}
+      config={{
+        loginMethods: ['email', 'wallet'],
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </PrivyProvider>
+  )
+}
