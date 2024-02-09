46258f49f6c1366b334fafcacffe0782128c4135
fahad <fahadu8@gmail.com>
2024-02-09 15:30:34 +0500

    remove duplicate

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,27 +1,7 @@
-import { PrivyProvider } from '@privy-io/react-auth'
-
 import type { AppProps } from 'next/app'
 
 require('dotenv').config({ path: ['.env.local', '.env'] })
 
-const handleLogin = (user: any) => {
-  console.log(`User ${user.id} logged in!`)
-}
 export const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-      onSuccess={handleLogin}
-      config={{
-        loginMethods: ['email', 'wallet'],
-        appearance: {
-          theme: 'light',
-          accentColor: '#676FFF',
-          logo: 'https://unsplash.com/photos/two-electronic-devices-sitting-next-to-each-other-VLZ6YOEIN-w',
-        },
-      }}
-    >
-      <Component {...pageProps} />
-    </PrivyProvider>
-  )
+  return <Component {...pageProps} />
 }
--- src/components/privyWrapper/PrivyWrapper.tsx
+++ src/components/privyWrapper/PrivyWrapper.tsx
@@ -6,8 +6,22 @@ import { FC, ReactNode } from 'react'
 
 const PrivyProviderWrapper: FC<{ children: ReactNode }> = ({ children }) => {
   const router = useRouter()
+  const handleSuccess = () => {
+    router.push('/dashboard')
+  }
   return (
-    <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''} onSuccess={() => router.push('/dashboard')}>
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
+      onSuccess={handleSuccess}
+      config={{
+        loginMethods: ['wallet'],
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://unsplash.com/photos/two-electronic-devices-sitting-next-to-each-other-VLZ6YOEIN-w',
+        },
+      }}
+    >
       {children}
     </PrivyProvider>
   )
