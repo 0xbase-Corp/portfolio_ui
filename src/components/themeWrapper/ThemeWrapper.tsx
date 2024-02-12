4e0fcb0955acc953f96d83aeb1cbee0f732ac068
ItsFaizan <faizanjavid71@gmail.com>
2024-02-12 23:44:59 +0500

    Toggle mode fixed 2

--- src/components/themeWrapper/ThemeWrapper.tsx
+++ src/components/themeWrapper/ThemeWrapper.tsx
@@ -4,35 +4,32 @@ import Brightness4Icon from '@mui/icons-material/Brightness4'
 import Brightness7Icon from '@mui/icons-material/Brightness7'
 import { Button } from '@mui/material'
 import { ThemeProvider } from '@mui/material/styles'
-import React, { FC, ReactNode, useState, useEffect } from 'react'
+import React, { FC, ReactNode, useEffect, useState } from 'react'
 
 import { darkTheme, lightTheme } from '@/utils/theme'
 
 const ThemeWrapper: FC<{ children: ReactNode }> = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false)
 
   useEffect(() => {
     // Check user preference from local storage or any other source
-    const storedTheme = localStorage.getItem('theme');
-    setIsDarkMode(storedTheme === 'dark');
-  }, []);
+    const storedTheme = localStorage.getItem('theme')
+    setIsDarkMode(storedTheme === 'dark')
+  }, [])
 
   const toggleTheme = () => {
-    const newTheme = isDarkMode ? 'light' : 'dark';
-    setIsDarkMode(!isDarkMode);
-    localStorage.setItem('theme', newTheme);
-  };
+    const newTheme = isDarkMode ? 'light' : 'dark'
+    setIsDarkMode(!isDarkMode)
+    localStorage.setItem('theme', newTheme)
+  }
 
-  const theme = isDarkMode ? darkTheme : lightTheme;
+  const theme = isDarkMode ? darkTheme : lightTheme
 
   return (
     <ThemeProvider theme={theme}>
       {children}
       <Button onClick={toggleTheme}>{isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}</Button>
     </ThemeProvider>
-    
-     
-    
   )
 }
 
