cd9fe3df706f2d82e31644ace301f9af004d4b40
ItsFaizan <faizanjavid71@gmail.com>
2024-02-23 23:17:48 +0500

    Backend end point fetched successfully

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,21 @@
+
 'use client'
 
 import React, { useEffect, useState } from 'react'
 
 import ResponsiveDrawer from '@/components/drawer/Drawer'
+import callApi from '@/utils/callApi'; 
 
 export default function Dashboard() {
   const [healthStatus, setHealthStatus] = useState('')
 
   useEffect(() => {
-    fetch('/healthy')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
-        }
-        return response.json()
+    callApi('/healthy', 'GET')
+      .then(data => {
+        const message = data.message; 
+        setHealthStatus(message);
       })
-      .then((data) => {
-        setHealthStatus(data)
-      })
-      .catch((error) => {
-        console.error('There was an error fetching the health status:', error)
-        setHealthStatus('Error fetching health status')
-      })
-  }, [])
+  }, []);
 
   return (
     <main>
--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -1,8 +1,10 @@
 import WaitlistForm from '@/containers/WaitlistForm'
+import Navbar from '@/components/navbar/Navbar'
 
 export default function waitlist() {
   return (
     <>
+      <Navbar />
       <WaitlistForm />
     </>
   )
