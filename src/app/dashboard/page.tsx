4a3b6db886d07607dc41cff733f1eb8b94a3fa55
ItsFaizan <faizanjavid71@gmail.com>
2024-03-04 05:35:49 +0500

    Removed extra things and prttier fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,36 +8,13 @@ import CardComponent from '@/components/card/Card'
 
 export default function Dashboard() {
   const [healthStatus, setHealthStatus] = useState('')
-  const [btcWalletData, setBtcWalletData] = useState([])
-  const [solanaWalletData, setSolanaWalletData] = useState([]);
+  
 
   useEffect(() => {
     callApi('/healthy', 'GET').then((data) => {
       const message = data.message
       setHealthStatus(message)
     })
-
-    const btcAddress = '37jKPSmbEGwgfacCr2nayn1wTaqMAbA94Z';
-    fetch(`http://localhost:5050/api/v1/portfolio/btc/${btcAddress}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setBtcWalletData(data);
-        console.log(data);
-      })
-      .catch((error) => console.error('Error fetching BTC wallet data:', error));
-    // const solanaAddress = 'ANRN6jr3auEieFNMvTG8C4ZBYLnUmceuFdNZnWfiYgr';
-    // fetch(`http://localhost:5050/api/v1/portfolio/solana/${solanaAddress}`, {
-    //   method: 'GET',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'x-api-key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImVkMDU0NGVhLWVkOGQtNDYxZS1hYmFiLTg1Y2IxNGQwN2NhOSIsIm9yZ0lkIjoiMzY2NTQ4IiwidXNlcklkIjoiMzc2NzEwIiwidHlwZUlkIjoiMjQ0ZjhlZjAtZTU5Ni00ZWMyLWIxY2MtYTExZDc4MjU3ZTI5IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MDE0MTQ1MDEsImV4cCI6NDg1NzE3NDUwMX0.tWhn6mb3vSpPbgXB_SYA7BD4c5Z0_lJ_Jsb73Nag824'
-    //   }
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //   setSolanaWalletData(data);
-    // })
-    // .catch(error => console.error('Error fetching Solana wallet data:', error));
   }, [])
 
   return (
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,33 +1,29 @@
-import React from 'react';
-import { Paper } from '@mui/material';
-import styled from 'styled-components';
-import { FC, ReactNode } from 'react';
+import { Paper } from '@mui/material'
+import React, { FC, ReactNode } from 'react'
+import styled from 'styled-components'
 
 const StyledPaper = styled(Paper)`
   padding: 20px;
   margin: 20px;
-  background-color: #1E1E2F;
-  color: #FFFFFF;
+  background-color: #1e1e2f;
+  color: #ffffff;
   border-radius: 10px;
-  box-shadow: 0 4px 20px 0 rgba(0,0,0,0.1);
+  box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.1);
   font-size: 10px;
-`;
-
+`
 
 const CardContainer = styled.div`
   display: flex;
-  justify-content: flex-end; 
-  width: 100%; 
-`;
+  justify-content: flex-end;
+  width: 100%;
+`
 
 const CardComponent: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <CardContainer>
-      <StyledPaper elevation={0}>
-        {children}
-      </StyledPaper>
+      <StyledPaper elevation={0}>{children}</StyledPaper>
     </CardContainer>
-  );
-};
+  )
+}
 
-export default CardComponent;
+export default CardComponent
