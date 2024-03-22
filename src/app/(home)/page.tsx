7394336fff1fc7f8bcca047bfcf14041b899ccf2
ItsFaizan <faizanjavid71@gmail.com>
2024-03-22 15:17:05 +0500

    Prettier fixed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,19 @@
 'use client'
 
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
+import IconButton from '@mui/material/IconButton'
 import { useState } from 'react'
 
 import Footer from '@/components/footer/Footer'
 import LoadingButton from '@/components/loadingbutton/LoadingButton'
 import Navbar from '@/components/navbar/Navbar'
-import LoginDialog from '@/views/LoginDialog'
 import { useTheme } from '@/components/themeWrapper/ThemeContext'
-import IconButton from '@mui/material/IconButton'
-import Brightness4Icon from '@mui/icons-material/Brightness4'
-import Brightness7Icon from '@mui/icons-material/Brightness7'
-
+import LoginDialog from '@/views/LoginDialog'
 
 export default function Home() {
   const [loading, setLoading] = useState(false)
-  const { isDarkMode, toggleTheme } = useTheme();
+  const { isDarkMode, toggleTheme } = useTheme()
   const handleButtonClick = async () => {
     try {
       setLoading(true)
--- src/components/themeWrapper/ThemeContext.tsx
+++ src/components/themeWrapper/ThemeContext.tsx
@@ -1,38 +1,35 @@
 'use client'
-import React, { createContext, useContext, useState, useEffect } from 'react';
+
+import React, { createContext, useContext, useEffect, useState } from 'react'
 
 interface ThemeContextType {
-  isDarkMode: boolean;
-  toggleTheme: () => void;
+  isDarkMode: boolean
+  toggleTheme: () => void
 }
 
-const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 export const useTheme = () => {
-  const context = useContext(ThemeContext);
+  const context = useContext(ThemeContext)
   if (!context) {
-    throw new Error('useTheme must be used within a ThemeProvider');
+    throw new Error('useTheme must be used within a ThemeProvider')
   }
-  return context;
-};
+  return context
+}
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false)
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem('theme');
-    setIsDarkMode(storedTheme === 'dark');
-  }, []);
+    const storedTheme = localStorage.getItem('theme')
+    setIsDarkMode(storedTheme === 'dark')
+  }, [])
 
   const toggleTheme = () => {
-    const newTheme = isDarkMode ? 'light' : 'dark';
-    setIsDarkMode(!isDarkMode);
-    localStorage.setItem('theme', newTheme);
-  };
+    const newTheme = isDarkMode ? 'light' : 'dark'
+    setIsDarkMode(!isDarkMode)
+    localStorage.setItem('theme', newTheme)
+  }
 
-  return (
-    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
+  return <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>{children}</ThemeContext.Provider>
+}
