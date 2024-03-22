1ab49b1c7c886ed759401e80db7fb85d4b83a66e
Muhammad Faizan Javed <95418879+ItsFaizan@users.noreply.github.com>
2024-03-22 22:25:20 +0500

    [PT-126]-StyleNavbar (#28)
    * Side bar styled
    
    * Hover effect added
    
    * useContext added and toggle button placed
    
    * Navbar Styled
    
    * Lint issues fixed
    
    * Image fixed
    
    * prettier fixed

--- src/components/drawer/DrawerAppBar.tsx
+++ src/components/drawer/DrawerAppBar.tsx
@@ -1,18 +1,21 @@
 'use client'
 
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import MenuIcon from '@mui/icons-material/Menu'
+import NotificationsIcon from '@mui/icons-material/Notifications'
 import SearchIcon from '@mui/icons-material/Search'
+import { Avatar, Menu, MenuItem } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import IconButton from '@mui/material/IconButton'
 import InputBase from '@mui/material/InputBase'
 import { alpha, styled } from '@mui/material/styles'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
-  borderRadius: theme.shape.borderRadius,
+  borderRadius: '20px',
   backgroundColor: alpha(theme.palette.common.white, 0.15),
   '&:hover': {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
@@ -22,6 +25,7 @@ const Search = styled('div')(({ theme }) => ({
   [theme.breakpoints.up('sm')]: {
     marginLeft: theme.spacing(1),
     width: 'auto',
+    minWidth: '20ch',
   },
 }))
 
@@ -43,12 +47,26 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     transition: theme.transitions.create('width'),
     width: '100%',
     [theme.breakpoints.up('sm')]: {
-      width: '12ch',
+      width: '16ch',
       '&:focus': {
         width: '20ch',
       },
     },
   },
+}))
+
+const UserSection = styled('div')(() => ({
+  display: 'flex',
+  alignItems: 'center',
+}))
+
+const NotificationButton = styled(IconButton)(({ theme }) => ({
+  borderRadius: '50%',
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  margin: theme.spacing(1),
 }))
 
 interface DrawerAppBarProps {
@@ -58,37 +76,92 @@ interface DrawerAppBarProps {
 }
 
 const DrawerAppBar: FC<DrawerAppBarProps> = ({ handleDrawerToggle, drawerWidth, title }) => {
-  return (
-    <AppBar
-      position="fixed"
-      sx={{
-        width: { sm: `calc(100% - ${drawerWidth}px)` },
-        ml: { sm: `${drawerWidth}px` },
-        bgcolor: 'background.default',
-        color: 'text.primary',
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null)
+  }
+
+  const isMenuOpen = Boolean(anchorEl)
+
+  const menuId = 'primary-search-account-menu'
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
       }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
     >
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: 'none' } }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-          {title}
-        </Typography>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
-        </Search>
-      </Toolbar>
-    </AppBar>
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  )
+
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          bgcolor: 'background.default',
+          color: 'text.primary',
+          boxShadow: 'none',
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div style={{ flexGrow: 1 }}>
+            <Typography variant="h6" noWrap component="div">
+              Hi,{title}
+            </Typography>
+            <Typography variant="subtitle2" noWrap component="div">
+              Here is your detailed portfolio view
+            </Typography>
+          </div>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search Anything…" inputProps={{ 'aria-label': 'search' }} />
+          </Search>
+          <NotificationButton>
+            <NotificationsIcon />
+          </NotificationButton>
+          <UserSection sx={{ gap: '8px' }}>
+            <Avatar />
+            <Typography variant="subtitle1" noWrap component="div" sx={{ marginRight: -2 }}>
+              {title}
+            </Typography>
+            <IconButton size="small" sx={{ cursor: 'pointer' }} onClick={handleProfileMenuOpen}>
+              <ArrowDropDownIcon />
+            </IconButton>
+          </UserSection>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </>
   )
 }
 
