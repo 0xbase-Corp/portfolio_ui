5abb0eaa5ea67abde98613518d3cb9ece5627227
ItsFaizan <faizanjavid71@gmail.com>
2024-03-22 15:10:00 +0500

    Header fixed

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,5 +1,5 @@
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp'
 import {
   Box,
   Collapse,
@@ -11,10 +11,10 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material'
-import React, { FC, useState } from 'react'
-import styled from 'styled-components'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import React, { FC, useState } from 'react'
+import styled from 'styled-components'
 
 const StyledTableContainer = styled(TableContainer)`
   position: fixed;
@@ -25,21 +25,22 @@ const StyledTableContainer = styled(TableContainer)`
   max-height: calc(100vh - 20px);
   overflow: auto;
 `
-const StyledHeaderCell = styled(TableCell)`
-  background-color: #121212; 
-  color: white;
-`;
 
 interface StyledTableRowProps {
   level: number;
+  isLastSubRow?: boolean;
 }
 
 const StyledTableRow = styled(TableRow)<StyledTableRowProps>`
+  & > * {
+    font-size: ${({ level }) => (level > 0 ? '0.875rem' : 'inherit')};
+  }
   & > *:first-child {
     position: relative;
     padding-left: ${({ level }) => `${level * 20}px`};
 
-    &:before, &:after {
+    &:before,
+    &:after {
       content: '';
       position: absolute;
       border-left: 2px solid #ccc;
@@ -53,26 +54,28 @@ const StyledTableRow = styled(TableRow)<StyledTableRowProps>`
     }
 
     &:after {
-      left: ${({ level }) => `${level * 90 - 10}px`};
+      
+      left: ${({ level }) => `${level * 130 - 10}%`};
+
       top: 0;
-      height: 100%;
+      height: ${({ isLastSubRow }) => (isLastSubRow ? '53%' : '100%')}; // Adjust height based on whether it's the last sub-row
     }
   }
-`;
+`
 
 interface CustomTableProps {
   data: any[]
 }
 
 interface RowData {
-  asset_name: string;
-  chain: string;
-  unit_price: number;
-  amount: number;
-  percentage: number;
-  profit_loss: number;
-  asset_price_usd: number;
-  subRows?: RowData[];
+  asset_name: string
+  chain: string
+  unit_price: number
+  amount: number
+  percentage: number
+  profit_loss: number
+  asset_price_usd: number
+  subRows?: RowData[]
 }
 
 const dummyData: RowData[] = [
@@ -83,7 +86,7 @@ const dummyData: RowData[] = [
     amount: 19.0,
     percentage: 6.52,
     profit_loss: -2.52,
-    asset_price_usd: 151.00,
+    asset_price_usd: 151.0,
     subRows: [
       {
         asset_name: 'Ethereum ETH Token',
@@ -92,7 +95,7 @@ const dummyData: RowData[] = [
         amount: 12.54,
         percentage: 2.79,
         profit_loss: 0.15,
-        asset_price_usd: 12.00,
+        asset_price_usd: 12.0,
       },
       {
         asset_name: 'rsw ETH Token',
@@ -101,11 +104,11 @@ const dummyData: RowData[] = [
         amount: 12.54,
         percentage: 1.25,
         profit_loss: 0.29,
-        asset_price_usd: 45.00,
+        asset_price_usd: 45.0,
       },
     ],
   },
- 
+
   {
     asset_name: 'Ethereum ETH2',
     chain: 'Mainnet',
@@ -113,7 +116,7 @@ const dummyData: RowData[] = [
     amount: 19.0,
     percentage: 6.52,
     profit_loss: -2.52,
-    asset_price_usd: 151.00,
+    asset_price_usd: 151.0,
     subRows: [
       {
         asset_name: 'Ethereum ETH Token',
@@ -122,7 +125,7 @@ const dummyData: RowData[] = [
         amount: 12.54,
         percentage: 2.79,
         profit_loss: 0.15,
-        asset_price_usd: 12.00,
+        asset_price_usd: 12.0,
       },
       {
         asset_name: 'rsw ETH Token',
@@ -131,7 +134,7 @@ const dummyData: RowData[] = [
         amount: 12.54,
         percentage: 1.25,
         profit_loss: 0.29,
-        asset_price_usd: 45.00,
+        asset_price_usd: 45.0,
       },
       {
         asset_name: 'rsw1 ETH Token',
@@ -140,7 +143,7 @@ const dummyData: RowData[] = [
         amount: 12.54,
         percentage: 1.25,
         profit_loss: 0.29,
-        asset_price_usd: 45.00,
+        asset_price_usd: 45.0,
       },
       {
         asset_name: 'rsw1 ETH Token',
@@ -149,11 +152,11 @@ const dummyData: RowData[] = [
         amount: 12.54,
         percentage: 1.25,
         profit_loss: 0.29,
-        asset_price_usd: 45.00,
+        asset_price_usd: 45.0,
       },
     ],
   },
-];
+]
 
 const CustomTable: FC<CustomTableProps> = ({ data }) => {
   const [open, setOpen] = useState<{ [key: string]: boolean }>({})
@@ -167,67 +170,96 @@ const CustomTable: FC<CustomTableProps> = ({ data }) => {
 
   return (
     <StyledTableContainer>
-     <Table aria-label="portfolio table" sx={{ backgroundColor: 'background.default' }}>
+      <Table aria-label="portfolio table" sx={{ backgroundColor: 'background.default' }}>
         <TableHead>
           <TableRow>
-            <TableCell /> 
-            <TableCell align='left'>Assets</TableCell>
+            <TableCell />
+            <TableCell style={{ position: 'relative', paddingBottom: 0 }}>
+            <Typography
+  variant="subtitle1"
+  sx={{
+    position: 'absolute',
+    left: '-4%',
+    transform: 'translateX(-50%)',
+    top: '28%',
+    '@media (max-width: 1150px)': {
+      top: '36%',
+    },
+  }}
+>
+  Assets
+</Typography>
+</TableCell>
             <TableCell align="left">Chain</TableCell>
-            <TableCell align="left">Unit Price</TableCell>
-            <TableCell align="left">Amount</TableCell>
+            <TableCell align="left">Price</TableCell>
+            <TableCell align="left">Quantity</TableCell>
             <TableCell align="left">Percentage</TableCell>
             <TableCell align="left">Profit/Loss</TableCell>
-            <TableCell align="left">Asset Price (USD)</TableCell>
-            <TableCell /> 
+            <TableCell align="left">Total Price (USD)</TableCell>
+            <TableCell />
           </TableRow>
         </TableHead>
         <TableBody>
           {dummyData.map((row) => (
-           <React.Fragment key={row.asset_name}>
-           <StyledTableRow level={0}>
-                <TableCell>
-                  <IconButton size="small" onClick={() => handleToggle(row.asset_name)}>
-                  {open[row.asset_name] ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+            <React.Fragment key={row.asset_name}>
+              <StyledTableRow level={0}>
+                <TableCell style={{ paddingRight: 0 }}>
+                  <IconButton
+                    size="small"
+                    onClick={() => handleToggle(row.asset_name)}
+                    style={{ marginRight: '-12px' }}
+                  >
+                    {open[row.asset_name] ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
                   </IconButton>
                 </TableCell>
-                <TableCell component="th" scope="row">{row.asset_name}</TableCell>
+                <TableCell component="th" scope="row" style={{ paddingLeft: '4px' }}>
+                  {row.asset_name}
+                </TableCell>
                 <TableCell align="left">{row.chain}</TableCell>
                 <TableCell align="left">{row.unit_price}</TableCell>
                 <TableCell align="left">{row.amount}</TableCell>
-                <TableCell align="left">{`${row.percentage}%`}</TableCell>
+                <TableCell align="left" style={{ color: 'green' }}>{`${row.percentage}%`}</TableCell>
                 <TableCell align="left" style={{ color: row.profit_loss >= 0 ? 'green' : 'red' }}>
-    <Box display="flex" alignItems="center" justifyContent="flex-start">
-      {row.profit_loss >= 0 ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
-      <Box marginLeft={0.5}>{`${row.profit_loss}%`}</Box>
-    </Box>
-  </TableCell>
+                  <Box display="flex" alignItems="center" justifyContent="flex-start">
+                    {row.profit_loss >= 0 ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+                    <Box marginLeft={0.5}>{`${row.profit_loss}%`}</Box>
+                  </Box>
+                </TableCell>
                 <TableCell align="left">{row.asset_price_usd}</TableCell>
                 <TableCell align="left">
                   <Button variant="contained" style={{ backgroundColor: 'green', color: 'white', borderRadius: 50 }}>
                     Forecast
-                  </Button> 
+                  </Button>
                 </TableCell>
-                </StyledTableRow>
-              {row.subRows && row.subRows.map((subRow, subIndex) => (
-                <StyledTableRow key={`${row.asset_name}-${subIndex}`} style={{ display: open[row.asset_name] ? 'table-row' : 'none' }} level={1}>
-                  <TableCell />
-                  <TableCell component="th" scope="row" style={{ paddingLeft: '50px' }}>
-                    <Typography variant="body2" color="textSecondary">{subRow.asset_name}</Typography>
-                  </TableCell>
-                  <TableCell align="left">{subRow.chain}</TableCell>
-                  <TableCell align="left">{subRow.unit_price}</TableCell>
-                  <TableCell align="left">{subRow.amount}</TableCell>
-                  <TableCell align="left">{`${subRow.percentage}%`}</TableCell>
-                  <TableCell align="left" style={{ color: subRow.profit_loss >= 0 ? 'green' : 'red' }}>
-                  <Box display="flex" alignItems="center" justifyContent="flex-start">
-                  {open[row.asset_name] ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
-                  <Box marginLeft={0.5}>{`${subRow.profit_loss}%`}</Box>
-                  </Box>
-                  </TableCell>
-                  <TableCell align="left">{subRow.asset_price_usd}</TableCell>
-                  <TableCell /> 
+              </StyledTableRow>
+              {row.subRows &&
+  row.subRows.map((subRow, subIndex) => (
+    <StyledTableRow
+      key={`${row.asset_name}-${subIndex}`}
+      style={{ display: open[row.asset_name] ? 'table-row' : 'none' }}
+      level={1}
+      isLastSubRow={subIndex === (row.subRows?.length ?? 0) - 1} // Pass the new prop here
+    >
+                    <TableCell />
+                    <TableCell component="th" scope="row" style={{ paddingLeft: '50px' }}>
+                      <Typography variant="body2" color="textSecondary">
+                        {subRow.asset_name}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="left">{subRow.chain}</TableCell>
+                    <TableCell align="left">{subRow.unit_price}</TableCell>
+                    <TableCell align="left">{subRow.amount}</TableCell>
+                    <TableCell align="left" style={{ color: 'green' }}>{`${subRow.percentage}%`}</TableCell>
+                    <TableCell align="left" style={{ color: subRow.profit_loss >= 0 ? 'green' : 'red' }}>
+                      <Box display="flex" alignItems="center" justifyContent="flex-start">
+                        {open[row.asset_name] ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+                        <Box marginLeft={0.5}>{`${subRow.profit_loss}%`}</Box>
+                      </Box>
+                    </TableCell>
+                    <TableCell align="left">{subRow.asset_price_usd}</TableCell>
+                    <TableCell />
                   </StyledTableRow>
-              ))}
+                ))}
             </React.Fragment>
           ))}
         </TableBody>
